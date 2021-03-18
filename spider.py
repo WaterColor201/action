f152f2a32c9bd4e19ff216d103ad8cf860c07889
skyone-wzw <2654431601@qq.com>
2021-03-18 20:03:39 +0800

    666

--- spider.py
+++ spider.py
@@ -8,8 +8,8 @@ res = requests.get(
             "format": "xml"
         }, )
 sel = html.fromstring(res.content)
-name = sel.xpath(r'//copyright/text()')[0].split(" ")[0].split("，")
-name = name[0] + '-' + name[1] + ".jpg"
+name = sel.xpath(r'//copyright/text()')[0].split(" ")[0]
+name = name + ".jpg"
 url = sel.xpath(r'//url/text()')[0].split("_")
 url = "https://cn.bing.com" + url[0] + '_' + url[1] + "_1920x1200.jpg"
 time = sel.xpath(r'//enddate/text()')[0]
