8970bf910676186068002c088622384a12eceb08
skyone-wzw <2654431601@qq.com>
2021-03-21 14:05:42 +0800

    666

--- bili/pipelines.py
+++ bili/pipelines.py
@@ -4,7 +4,7 @@ from os import path
 
 class BiliPipeline:
     def process_item(self, item, spider):
-        name = item["title"] + '-' + str(item["id"]) + '.' + item["extension"]
+        name = str(item["id"]) + '.' + item["extension"]
         res = requests.get(item["url"])
         with open(path.join(path.join(path.abspath('.'), 'image'), name), 'wb') as file:
             file.write(res.content)
